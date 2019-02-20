6edd7793a0cc56f5cd3dc81a067dab129f1f0b54
Cole Greenlee <ColeTGreenlee@gmail.com>
2019-02-20 17:22:29 -0600

    Added Project for Console Testing of API Project

---
+++ GopherTrip.ConsoleTests/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GopherTrip.ConsoleTests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
