31df4b211165bbc73dee36f3c4c707e4b4bdaaf2
Cole Greenlee <ColeTGreenlee@gmail.com>
2019-02-20 17:25:11 -0600

    Created classes from sample data
    Also added a Util class that is holding Serialize and Converter methods

---
+++ GopherTrip.API/Util.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace GopherTrip.API
+{
+    class Util
+    {
+        //Blank for now.
+    }
+    public static class Serialize
+    {
+        public static string ToJson(this Stops self) => JsonConvert.SerializeObject(self, GopherTrip.API.Converter.Settings);
+    }
+
+    internal static class Converter
+    {
+        public static readonly JsonSerializerSettings Settings = new JsonSerializerSettings
+        {
+            MetadataPropertyHandling = MetadataPropertyHandling.Ignore,
+            DateParseHandling = DateParseHandling.None,
+            Converters =
+            {
+                new IsoDateTimeConverter { DateTimeStyles = DateTimeStyles.AssumeUniversal }
+            },
+        };
+    }
+}
